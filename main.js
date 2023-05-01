736cd1fa736e286804441b03bda8884eaa4116a3
ktrnaaa <120386793+ktrnaaa@users.noreply.github.com>
2023-05-01 02:19:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+function Validation(){
+
+return typeof number === 'number' && !isNaN(number);
+
+}
+
+
+
+
 function addThemAll(){
 
     let result = 0;
-
+   
     for (let i = 0; i < arguments.length; i++) {
-      result += arguments[i];
+      const number = arguments[i];
+
+      if (Validation(number)){
+      result += number;}
     }
   
     return result;
@@ -19,9 +31,19 @@ console.log(addThemAll(5,5,10)); // 20*
 
 function multiply(a) {
 
+  if (!Validation(a)){
+
+    return 'Invalid number';
+  }
+
   return function(b){
 
-    return a*b;
+    if (!Validation(b)){
+
+      return 'Invalid number';
+    }
+
+    return a * b;
   }
 
 }
@@ -59,15 +81,46 @@ const movies = [
     ];
     
 
-    
-    function byProperty(property, direction) {
+    const sort_property = {
 
-     if (direction === '>'){
-       movies.sort((a, b) => a[property] > b[property] ? 1 : -1);}
-       else if (direction === '<'){
-        movies.sort((a, b) => a[property] > b[property] ? -1 : 1);
-       }
-      return 
+      name: 'movieName',
+      release: 'releaseYear',
+      director: 'directedBy',
+      duration: 'runTimeInMinutes'
+
+    };
+
+    const sort_order = {
+
+      asc: '>',
+      desc: '<'
+    }
+
+    const sort_order_values = {
+
+      [sort_order.asc]: 1,
+      [sort_order.desc]: -1
+    }
+
+    function byProperty(property, direction) {
+      const validProperties = Object.values(sort_property);
+
+
+      if (!validProperties.includes(property)){
+        return 'Invalid property';
+
+      }
+
+      const validOrders = Object.values(sort_order);
+
+      if (!validOrders.includes(direction)){
+
+        return 'Invalid direction'
+      }
+
+      const orderValue = sort_order_values[direction];
+
+      return (a, b) => a[property] ? orderValue : -1 * orderValue;
     }
     console.log(movies.sort(byProperty('releaseYear', '>'))); // виведе масив фільмів посортованих по року випуску, від старішого до новішого*
     console.log(movies.sort(byProperty('runningTimeInMinutes', '<'))); // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого*
